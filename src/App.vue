718938fdda47b6e027ba0d47a70a9443caaff6e7
Tanuj <tanujnagpal999@gmail.com>
2019-01-22 22:50:00 +0530

    Added animations

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
-<template><div id="app">
-<router-link class="nav-link" to="/adder">Adder</router-link>
-  <router-link class="nav-link" to="/subtracter">Subtracter</router-link>
-  <keep-alive>
-  <router-view/>
-  </keep-alive>
+<template>
+  <div id="app">
+    <router-link class="nav-link" to="/adder">Adder</router-link>
+    <router-link class="nav-link" to="/subtracter">Subtracter</router-link>
+    <keep-alive>
+      <transition name="component-fade" mode="out-in">
+        <router-view/>
+      </transition>
+  
+    </keep-alive>
   </div>
 </template>
 
@@ -15,3 +19,17 @@
   }
 </script>
 
+<style lang="">
+  .component-fade-enter-active,
+  .component-fade-leave-active {
+    transition: opacity .3s ease;
+  }
+  
+  .component-fade-enter,
+  .component-fade-leave-to
+  /* .component-fade-leave-active below version 2.1.8 */
+  
+  {
+    opacity: 0;
+  }
+</style>
